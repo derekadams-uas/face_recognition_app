15a3e49a80b57c08f30b0dae2660a52198b05f75
Derek Adams <derekadams.uas@gmail.com>
2019-12-27 22:06:18 -0700

    added image box

--- face_recognition_app/src/App.js
+++ face_recognition_app/src/App.js
@@ -34,18 +34,18 @@ class App extends Component {
   }
 
   onInputChange = (event) => {
-    this.setState({ input: event.target.value })
+    this.setState({ input: event.target.value });
   }
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.input });
+    this.setState({ imageUrl: this.state.input });
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(
         function (response) {
-          // do something with response
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         },
         function (err) {
           // there was an error
