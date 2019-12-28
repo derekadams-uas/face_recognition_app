cbc8e5f54ae79f11da4cc1755736f561289df071
Derek Adams <derekadams.uas@gmail.com>
2019-12-27 21:01:44 -0700

    added rank and particles in background

--- face_recognition_app/src/App.js
+++ face_recognition_app/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition />*/}
     </div>
