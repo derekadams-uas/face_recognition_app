fe5a4b02cc303accbe09bf0777f45e2cae55dc99
Derek Adams <derekadams.uas@gmail.com>
2019-12-29 21:29:53 -0700

    added user registration

--- face_recognition_app/src/App.js
+++ face_recognition_app/src/App.js
@@ -34,7 +34,14 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
@@ -42,6 +49,18 @@ class App extends Component {
     fetch('http://localhost:3000/')
       .then(response => response.json())
       .then(console.log);
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
   }
 
   calculateFaceLocation = (data) => {
@@ -104,7 +123,7 @@ class App extends Component {
           (
             route === 'signin'
               ? <SignIn onRouteChange={this.onRouteChange} />
-              : <Register onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
         }
       </div>
