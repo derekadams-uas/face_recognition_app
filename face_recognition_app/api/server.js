9c00c16e95525d64ab7d8c728157e56f967914e7
Derek Adams <derekadams.uas@gmail.com>
2019-12-29 20:58:30 -0700

    added fetch in app.js

--- face_recognition_app/api/server.js
+++ face_recognition_app/api/server.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
         {
             id: '123',
             name: 'John',
-            email: 'john@gmail.com',
             password: 'cookies',
+            email: 'john@gmail.com',
             entries: 0,
             joined: new Date()
         },
         {
             id: '124',
             name: 'Sally',
-            email: 'sally@gmail.com',
             password: 'bananas',
+            email: 'sally@gmail.com',
             entries: 0,
             joined: new Date()
         }
@@ -30,6 +34,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+    // bcrypt.compare("bacon", hash, function (err, res) {
+    //     // res == true
+    // });
+    // bcrypt.compare("veggies", hash, function (err, res) {
+    //     // res = false
+    // });
     if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
         res.json('success');
     } else {
@@ -39,6 +49,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function (err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: '125',
         name: name,
@@ -82,11 +95,3 @@ app.put('/image', (req, res) => {
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 })
-
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT = user
-*/
