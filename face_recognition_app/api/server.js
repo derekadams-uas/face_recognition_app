bef4102a6ec36195a64a4311e0282748ecf019c6
Derek Adams <derekadams.uas@gmail.com>
2019-12-30 17:34:39 -0700

    added controllers for signin and register

--- face_recognition_app/api/server.js
+++ face_recognition_app/api/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
 
 const db = knex({
     client: 'pg',
@@ -22,52 +24,9 @@ app.get('/', (req, res) => {
     res.send(db.users);
 })
 
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('Unable to Get User'))
-            } else {
-                res.status(400).json('Wrong Credentials');
-            }
-        })
-        .catch(err => res.status(400).json('Wrong Credentials'))
-})
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        email: loginEmail[0],
-                        name: name,
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0]);
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback);
-    })
-        .catch(err => res.status(400).json('Unable to Register'));
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
--- face_recognition_app/src/App.js
+++ face_recognition_app/src/App.js
@@ -26,23 +26,25 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   componentDidMount() {
@@ -102,7 +104,8 @@ class App extends Component {
             .then(response => response.json())
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -111,9 +114,9 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false })
+      this.setState(initialState);
     } else if (route === 'home') {
-      this.setState({ isSignedIn: true })
+      this.setState({ isSignedIn: true });
     }
     this.setState({ route: route });
   }
