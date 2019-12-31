41080c29d22023872904339141d287b3777eefaa
Derek Adams <derekadams.uas@gmail.com>
2019-12-30 17:07:36 -0700

    updated sign in function

--- face_recognition_app/api/server.js
+++ face_recognition_app/api/server.js
@@ -18,29 +18,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            password: 'cookies',
-            email: 'john@gmail.com',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            password: 'bananas',
-            email: 'sally@gmail.com',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send(db.users);
 })
 
 app.post('/signin', (req, res) => {
