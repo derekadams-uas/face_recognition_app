abf769e8c89538a86f927978b7939774dc2a139d
Derek Adams <derekadams.uas@gmail.com>
2019-12-30 21:55:17 -0700

    removed didmount

--- face_recognition_app/src/App.js
+++ face_recognition_app/src/App.js
@@ -42,12 +42,6 @@ class App extends Component {
     this.state = initialState;
   }
 
-  componentDidMount() {
-    fetch('https://radiant-harbor-91047.herokuapp.com/')
-      .then(response => response.json())
-      .then(console.log);
-  }
-
   loadUser = (data) => {
     this.setState({
       user: {
