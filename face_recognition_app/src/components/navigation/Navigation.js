069b30ff35077ac405b6088697257dbb50c82e8a
Derek Adams <derekadams.uas@gmail.com>
2019-12-30 23:14:04 -0700

    adjusted nav styling

--- face_recognition_app/src/components/navigation/Navigation.js
+++ face_recognition_app/src/components/navigation/Navigation.js
@@ -3,16 +3,22 @@ import React from 'react';
 const Navigation = ({ onRouteChange, isSignedIn }) => {
     if (isSignedIn) {
         return (
-            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <p onClick={() => onRouteChange('signout')} className='f6 link dim black pa3 pointer'>Sign Out</p>
-            </nav>
+            <div>
+                <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <p className='f6 white pa3'>Face Detection App</p>
+                    <p onClick={() => onRouteChange('signout')} className='f6 link dim white pa3 pointer'>Sign Out</p>
+                </nav>
+            </div >
         );
     } else {
         return (
-            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <p onClick={() => onRouteChange('signin')} className='f6 link dim black pa3 pointer'>Sign In</p>
-                <p onClick={() => onRouteChange('register')} className='f6 link dim black pa3 pointer'>Register</p>
-            </nav>
+            <div>
+                <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <p className='f6 black pa3 b--solid'>Face Detection App</p>
+                    <p onClick={() => onRouteChange('signin')} className='f6 link dim black pa3 pointer'>Sign In</p>
+                    <p onClick={() => onRouteChange('register')} className='f6 link dim black pa3 pointer'>Register</p>
+                </nav>
+            </div>
         );
     }
 }
