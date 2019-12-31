b38cb5f1d8b2d6deb8ff4e5a210e90ebb8f11a61
Derek Adams <derekadams.uas@gmail.com>
2019-12-30 20:53:35 -0700

    adjusted server port settings

--- face_recognition_app/api/server.js
+++ face_recognition_app/api/server.js
@@ -29,6 +29,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(3000, () => {
-    console.log('App is Running on Port 3000');
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is Listening on port ${PORT}`);
 })
