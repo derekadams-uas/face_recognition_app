4be84e0cdbc3428666fa653023693952528ac17d
Derek Adams <derekadams.uas@gmail.com>
2020-01-14 09:52:14 -0700

    added dropdown toggle

--- face_recognition_app/src/App.js
+++ face_recognition_app/src/App.js
@@ -24,8 +24,8 @@ const initialState = {
   input: '',
   imageUrl: '',
   boxes: [],
-  route: 'signin',
-  isSignedIn: false,
+  route: 'home',
+  isSignedIn: true,
   user: {
     id: '',
     name: '',
